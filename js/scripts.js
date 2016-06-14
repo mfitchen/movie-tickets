abf43039f05ba3b795b76a52be0b25317f40d75a
Marty Fitchen and Matt Wilkin <matt@>
2016-06-14 16:20:21 -0700

    add poster() prototype to the business end

--- js/scripts.js
+++ js/scripts.js
@@ -11,13 +11,13 @@ Ticket.prototype.price = function() {
   if(this.movie === "Finding Dory" || this.movie === "Zootopia" || this.movie === "X-Men: Apocalypse") {
     price *= 2;
   } else {
-    price *=1;
+    price *= 1;
   };
 
   if(this.time === "11am" || this.time === "12pm" || this.time === "1pm" || this.time === "2pm" || this.time === "3pm" || this.time === "4pm") {
     price *= 1;
   } else {
-    price *=2;
+    price *= 2;
   };
 
   if(this.age === "Senior") {
@@ -31,6 +31,24 @@ Ticket.prototype.price = function() {
   return price;
 };
 
+Ticket.prototype.poster = function() {
+  var poster;
+
+  if(this.movie === "Finding Dory") {
+    poster = "finding-dory.jpg";
+  } else if (this.movie === "Zootopia") {
+    poster = "zootopia.jpg";
+  } else if (this.movie === "X-Men: Apocalypse") {
+    poster = "x-men.jpg";
+  } else {
+    poster = "deadpool.jpg";
+  };
+
+  return poster;
+};
+
+
+
 // user interface logic
 $(document).ready(function() {
   $("form#new-ticket").submit(function(event) {
@@ -41,30 +59,16 @@ $(document).ready(function() {
     var inputtedAge = $("input:radio[name=age]:checked").val();
     var newTicket = new Ticket(inputtedMovieName, inputtedMovieTime, inputtedAge);
 
-    var poster;
-
-    if(inputtedMovieName === "Finding Dory") {
-      poster = "finding-dory.jpg";
-    } else if (inputtedMovieName === "Zootopia") {
-      poster = "zootopia.jpg";
-    } else if (inputtedMovieName === "X-Men: Apocalypse") {
-      poster = "x-men.jpg";
-    } else {
-      poster = "deadpool.jpg";
-    };
-
     $("ul#ticketPrice").append("<li><span class='ticketName'>" + newTicket.movie + "</span></li>");
 
     $("#ticketPrice").last().click(function() {
       $("#show-ticket").show();
-      $("#show-ticket h2").html('<img src="img/' + poster + '">');
+      $("#show-ticket h2").html('<img src="img/' + newTicket.poster() + '">');
       $(".movie-name").text(newTicket.movie);
       $(".movie-time").text(newTicket.time);
       $(".purchaser-age").text(newTicket.age);
       $(".ticket-price").text(newTicket.price());
     });
 
-    $(".new-address").not("#new-address").hide();
-    // resetFields();
   });
 });
